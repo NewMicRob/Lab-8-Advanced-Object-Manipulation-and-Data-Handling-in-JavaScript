f0a7799e303c28a180ceb8758db1e5655a587977
NewMicRob <microbnew@gmail.com>
2025-04-04 22:53:16 -0400

    Part 1

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 //Part 1: Understanding and Creating Objects
-let student = {
-    name: "jeff",
-    age: 20,
-    enrolled: true,
-    courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"],
-    studentInfo: function() {
+let student = { // Creating an object student
+    name: "jeff", // Object name property
+    age: 20, // Object age property
+    enrolled: true, // Object enrolled property(boolean)
+    courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"], // Object courses property(array)
+    studentInfo: function() { 
+        console.log('Name: ' + this.name, 'Age: ' + this.age); // the output
+    }
+};
+
+student.studentInfo(); // Calling the studentInfo method
+
+//Part 2: Working with JSON
