e6a54d41acc3e2047b9df6b226d1c8413db8d762
NewMicRob <microbnew@gmail.com>
2025-04-05 00:01:00 -0400

    Part 2

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ let student = { // Creating an object student
     enrolled: true, // Object enrolled property(boolean)
     courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"], // Object courses property(array)
     studentInfo: function() { 
-        console.log('Name: ' + this.name, 'Age: ' + this.age); // the output
+        console.log('Name: ' + this.name + '\n' + 'Age: ' + this.age); // the output
     }
 };
 
 student.studentInfo(); // Calling the studentInfo method
 
 //Part 2: Working with JSON
+let jsonString = '{"name": "jeff", "age": 20, "enrolled": true, "courses": ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"]}'; // JSON string
+console.log(jsonString); // Logging the json string
+let jsonObject = JSON.parse(jsonString); // Pback inot an object
+console.log(jsonObject); // Logging the json object
+
+//Part 3: Using destructuring assignment
