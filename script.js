d2bdc0226ffe80ee411159b8f8296620df1e9c45
NewMicRob <microbnew@gmail.com>
2025-04-04 22:31:33 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Part 1: Understanding and Creating Objects
+let student = {
+    name: "jeff",
+    age: 20,
+    enrolled: true,
+    courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"],
+    studentInfo: function() {
