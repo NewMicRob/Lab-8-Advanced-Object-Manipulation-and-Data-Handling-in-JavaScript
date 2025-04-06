9066e2d50c582f0ebdb9f2120bcb8b8bde577409
NewMicRob <microbnew@gmail.com>
2025-04-06 01:37:32 -0400

    Part 5.2: added course count to student and moved add course method into student.

--- script.js
+++ script.js
@@ -1,47 +1,54 @@
 //Part 1: Understanding and Creating Objects
-let student = { // Creating an object student
-    name: "Jeff", // Object name property
-    age: 20, // Object age property
-    enrolled: true, // Object enrolled property(boolean)
-    courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"], // Object courses array
+let student = { 
+    name: "Jeff", 
+    age: 20, 
+    enrolled: true, 
+    courses: ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"], 
     studentInfo: function() { 
-        console.log('Name: ' + this.name + '\n' + 'Age: ' + this.age); // the output
+        console.log('Name: ' + this.name + '\n' + 'Age: ' + this.age); 
+    },
+
+    //Part 5: Object Methods
+    //Part 5.1
+    addCourse: function(course) {
+        this.courses.push(course); 
+    },
+    //Part 5.2
+    courseCount: function() {
+        return this.courses.length; s
     }
 };
-student.studentInfo(); // Calling the studentInfo method
+student.studentInfo(); 
+
 
 //Part 2: Working with JSON
-let jsonString = '{"name": "jeff", "age": 20, "enrolled": true, "courses": ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"]}'; // JSON string
-console.log(jsonString); // Logging the json string
-let jsonObject = JSON.parse(jsonString); // Pback inot an object
-console.log(jsonObject); // Logging the json object
+let jsonString = '{"name": "jeff", "age": 20, "enrolled": true, "courses": ["Algo", "JavaScript", "Web Dev", "Writ", "OS", "Databases"]}'; 
+console.log(jsonString); 
+let jsonObject = JSON.parse(jsonString); 
+console.log(jsonObject); 
 
 //Part 3: Using destructuring assignment
-var { name, courses } = student; // Destructuring the student object
-console.log(name); // Logging the name
-console.log(courses); // Logging the courses
+var { name, courses } = student; 
+console.log(name); 
+console.log(courses); 
 
-const scores = [85, 92, 78, 90]; // Array of scores
-const [firstScore, secondScore] = scores; // Destructuring array
-console.log(firstScore, secondScore); // logging the first and second scores
+const scores = [85, 92, 78, 90]; 
+const [firstScore, secondScore] = scores; 
+console.log(firstScore, secondScore); 
 
 //Part 4: The spread operator
-const studentClone = { ...student }; // Using spread operator to copy the student object
-const modifiedClone = { ...studentClone, GraduationYear: 2026}; // Modifying the copied object
-//console.log(studentClone); // Logging the cloned object
-console.log(modifiedClone); // Logging the modified cone object
-const newCourses = ["Math", "French", "Front-End", "Writ", "Art", "Databases"]; // New Array of courses
-const allCourses = [...student.courses, ...newCourses]; // Using spread operator to combine arrays
-console.log(allCourses); // Logging the combined array
-modifiedClone.courses = allCourses; // Assigning the combined array to the student object
-console.log(modifiedClone); // Logging the updated courses array
+const studentClone = { ...student }; 
+const modifiedClone = { ...studentClone, GraduationYear: 2026}; 
+//console.log(studentClone); 
+console.log(modifiedClone); 
+const newCourses = ["Math", "French", "Front-End", "Writ", "Art", "Databases"]; 
+const allCourses = [...student.courses, ...newCourses]; 
+console.log(allCourses);
+modifiedClone.courses = allCourses; 
+console.log(modifiedClone); 
 
-//Part 5:Object Methods
-function addCourse(course) {
-    this.courses.push(course); // Adding a course to the courses array
-}
-console.log(student.courses);
-addCourse.call(student, "Python"); //Adding a course
-console.log(student.courses);
-console.log(student);
- //Upadtreed course array
+//logs for part 5
+student.addCourse("Python"); //To add a course
+console.log("Total courses:", student.courseCount()); //count the number og courses
+student.addCourse("Java"); //Add anouther
+console.log("Total courses:", student.courseCount()); //count again
